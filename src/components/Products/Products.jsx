881e93a90dbb783350e58bd376984d2652081043
Jessica Lennick <77980935+jesslenn@users.noreply.github.com>
2022-01-21 17:46:16 -0500

    Add: EOD commit. More build out on Product.jsx and Products.jsx

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import Grid from '@material-ui/core'
 
 const products = [
-  {id: 1, name: 'shoes', description: 'bright red stiletto heels'}, 
-  {id: 2, name: 'blouse', description: 'sheer, flowery, cottagecore fastasy. long sleeves'},
-  {id: 3, name: 'skirt', description: 'broomstick skirt, perfect for swishing over tall fields of grass'},
+  {id: 1, name: 'shoes', description: 'bright red stiletto heels', price: '100'}, 
+  {id: 2, name: 'blouse', description: 'sheer, flowery, cottagecore fastasy. long sleeves', price: '50'},
+  {id: 3, name: 'skirt', description: 'broomstick skirt, perfect for swishing over tall fields of grass', price: '65'},
 ]
 
 const Products = () => {
@@ -12,7 +12,7 @@ const Products = () => {
     <Grid container justify="center" spacing={4}>
       {products.map((product) => (
         <Grid item key = {product.id} xs={12} sm={6} md={4} lg={3}>
-          <Product />
+          <Product product={product}/>
         </Grid>
       ))}
     </Grid>
