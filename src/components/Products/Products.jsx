f7666f978fbce2600551a2a28d4343af14371cc7
Jessica Lennick <77980935+jesslenn@users.noreply.github.com>
2022-01-22 18:50:43 -0500

    Add: Navbar.jsx, fixed styling and images on Prodcts

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import Grid from '@material-ui/core'
-import Product from './Product'
+import { Grid } from '@material-ui/core'
+import Product from './Product/Product'
 
 const products = [
-  {id: 1, name: 'shoes', description: 'bright red stiletto heels', price: '100'}, 
-  {id: 2, name: 'blouse', description: 'sheer, flowery, cottagecore fastasy. long sleeves', price: '50'},
-  {id: 3, name: 'skirt', description: 'broomstick skirt, perfect for swishing over tall fields of grass', price: '65'},
+  {id: 1, name: 'shoes', description: 'bright red stiletto heels', price: '100', image: 'https://tinyurl.com/2p89b47x'}, 
+  {id: 2, name: 'blouse', description: 'sheer, flowery, cottagecore fastasy. long sleeves', price: '50', image: 'https://tinyurl.com/muzeyn9r'},
+  {id: 3, name: 'skirt', description: 'broomstick skirt, perfect for swishing over tall fields of grass', price: '65', image: 'https://tinyurl.com/2mmswndk'},
 ]
 
 const Products = () => {
-  <main>
+  return (
+    <main>
     <Grid container justify="center" spacing={4}>
       {products.map((product) => (
         <Grid item key = {product.id} xs={12} sm={6} md={4} lg={3}>
@@ -18,7 +19,7 @@ const Products = () => {
       ))}
     </Grid>
   </main>
-
+  )
 }
 
 export default Products
