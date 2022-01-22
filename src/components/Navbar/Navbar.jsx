d37414fcfbe3c9876032a83da740bf93c245e1bd
Jessica Lennick <77980935+jesslenn@users.noreply.github.com>
2022-01-22 18:58:40 -0500

    Add: assets/logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
+import { ShoppingCart } from '@material-ui/icons'
+
+import logo from '../../assets/commerce.png'
 
 const Navbar = () => {
   return (
-    <div>
+    <>
+    <AppBar position="fixed" className={classes.appBar} color='inherit'>
+      <Toolbar>
+        <Typography variant='h6' className={classes.title} color='inherit'>
+          <img src={logo} alt='Commerce.js' height='25px' className={classes.image}/>
+          This is my teeeext
+        </Typography>
+      </Toolbar>
+    </AppBar>
 
-    </div>
+    </>
   )
 }
 
