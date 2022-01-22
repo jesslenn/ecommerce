6550cdef5e7cf3a78cb324275ffee88a418eef43
Jessica Lennick <77980935+jesslenn@users.noreply.github.com>
2022-01-22 18:36:44 -0500

    Add: Product/styles.js Added all styling for Product.js

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Card, CardMedia, CardContent, CardActions, Typography, IconButton} from '@material-ui/core';
 import { AddShoppingCart } from '@material-ui/icons';
+import useStyles from './styles'
 
 const Product = ({product}) => {
+  const classes = useStyles();
   return (
     <Card className={classes.root}>
       <CardMedia className={classes.media} image='' title={product.name}/>
@@ -15,7 +17,15 @@ const Product = ({product}) => {
             {product.price}
           </Typography>
         </div>
+        <Typography variant='h2' color='textSecondary'>
+          {product.description}
+        </Typography>
       </CardContent>
+      <CardActions disableSpacing className={classes.cardActions}>
+        <IconButton aria-label="Add to Cart">
+          <AddShoppingCart />
+        </IconButton>
+      </CardActions>
     </Card>
   )
 }
