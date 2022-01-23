be803a8f2ab325e5996fbce800952b0ef65834dc
Jessica Lennick <77980935+jesslenn@users.noreply.github.com>
2022-01-23 15:50:36 -0500

    Add: finished styling for Navbar.jsx

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Grid } from '@material-ui/core'
 import Product from './Product/Product'
+import useStyles from './styles'
 
 const products = [
   {id: 1, name: 'shoes', description: 'bright red stiletto heels', price: '100', image: 'https://tinyurl.com/2p89b47x'}, 
@@ -9,8 +10,10 @@ const products = [
 ]
 
 const Products = () => {
+  const classes = useStyles();
   return (
-    <main>
+    <main className={classes.content}>
+      <div className={classes.toolbar}/>
     <Grid container justify="center" spacing={4}>
       {products.map((product) => (
         <Grid item key = {product.id} xs={12} sm={6} md={4} lg={3}>
