55ec89d774ea5fa8a9e37918988d1aa3f84e9735
Jessica Lennick <77980935+jesslenn@users.noreply.github.com>
2022-01-23 17:39:56 -0500

    Add: Commerce keys, fetchProducts function using commerceAPI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Products, Navbar } from './components'
+import {commerce} from './lib/commerce'
 
 const App = () => {
+  const [products, setProducts] = useState([])
+
+  //we do an asynchronous call to commerce to fetch all our products from their DB
+  const fetchProducts = async () => {
+    const {data} = await commerce.products.list();
+    setProducts(data);
+  }
+
+  //useEffect with an empty array as its second argument will be called when the page is loaded
+useEffect(() => {
+  fetchProducts();
+}, [])
+
   return (
     <div>
       <Navbar />
